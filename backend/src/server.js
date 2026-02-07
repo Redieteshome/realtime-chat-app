aab69615e7fabcf46f9c54b7abc8554f3ecb4696
Rediet Teshome Tadesse <redieteshome78@gmail.com>
2026-02-07 12:51:14 +0300

    refactor: separated route logic from server entry file to make it cleaner

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 
-const app = express();
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 
 dotenv.config();
 
+const app = express();
+
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/api/login", (req, res) => {
-    res.send("Login Endpoint")
-})
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server running on port: " + PORT);
