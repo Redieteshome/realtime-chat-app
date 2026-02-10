aca72c5ff930c0a52c88e4fdc441285d78bdaa52
Rediet Teshome Tadesse <redieteshome78@gmail.com>
2026-02-10 23:18:22 +0300

    rate limiting for requests implemented

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -7,11 +7,11 @@ import {
 } from "../controllers/auth.controller.js";
 
 import { protectRoute } from "../middleware/auth.middleware.js";
-// import { arcjetProtection } from "../middleware/arcjet.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
-// router.use(arcjetProtection);
+router.use(arcjetProtection);
 
 router.post("/signup", signup);
 router.post("/login", login);
